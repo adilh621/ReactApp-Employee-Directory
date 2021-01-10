f0a14eb0f20f5e4a9fb5d88ce3ec2d098fd0fe28
Adil Hussain <adilh621@gmail.com>
2021-01-09 19:13:17 -0500

    finished app features

--- employee-directory/src/components/UserView/index.js
+++ employee-directory/src/components/UserView/index.js
@@ -1,29 +1,22 @@
 /* eslint-disable jsx-a11y/img-redundant-alt */
-import React, { Component } from "react";
-// import "./style.css";
+import React from "react";
+import "./style.css";
 
-class UserView extends Component {
-    state = {
-        searchResults: [],
-        filteredResults: [],
-        password: ""
-      };
-      render (props){
-          return(
+
+function UserView (props){
+
+    return (
   
-        <div>
-          <div class="row">
-            <div class="col user-images"><img src={props.user.picture.thumbnail} alt="User Photo" style= {{ width:"30%"}}></img></div>
-            <div class="col"><p>{props.user.name.first} {props.user.name.last}</p></div>
-            <div class="col"><p>{props.user.phone}</p></div>
-            <div class="col"><p>{props.user.email}</p></div>
-            <div class="col"><p>{props.user.dob.date.substring(0, 10)}</p></div>
-          </div>
+      <div>
+        <div class="row">
+          <div class="col user-images"><img src={props.user.picture.thumbnail} alt="User Photo" style= {{ width:"45%"}}></img></div>
+          <div class="col"><p>{props.user.name.first} {props.user.name.last}</p></div>
+          <div class="col"><p>{props.user.phone}</p></div>
+          <div class="col"><p>{props.user.email}</p></div>
+          <div class="col"><p>{props.user.dob.date.substring(0, 10)}</p></div>
         </div>
-          )
-      };
-    
-    
-}
+      </div>
+      );
+  }
 
 export default UserView;
